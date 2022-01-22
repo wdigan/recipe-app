bfdb60436481a7371bae73201dfd866784744dc1
wdigan <william.digan.work@gmail.com>
2022-01-22 10:23:31 +0100

    correct typo

--- app/app/settings.py
+++ app/app/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),s
+        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
